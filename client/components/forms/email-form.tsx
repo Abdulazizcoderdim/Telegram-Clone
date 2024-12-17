7a8dc5b140eddca4f5b9284034e2ecec3929565d
AbdulazizRus <coderdim@gmail.com>
2024-12-18 00:53:20 +0500

    email settings add

--- client/components/forms/email-form.tsx
+++ client/components/forms/email-form.tsx
@@ -3,6 +3,10 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { Button } from '../ui/button';
+import { Form, FormControl, FormField, FormItem, FormMessage } from '../ui/form';
+import { Input } from '../ui/input';
+import { Label } from '../ui/label';
 
 const EmailForm = () => {
   const [verify, setVerify] = useState(false);
@@ -11,7 +15,7 @@ const EmailForm = () => {
     resolver: zodResolver(oldEmailSchema),
     defaultValues: {
       email: '',
-      oldEmail: '',
+      oldEmail: 'info@exmaple.com',
     },
   });
 
@@ -23,7 +27,50 @@ const EmailForm = () => {
     },
   });
 
-  return <div>EmaiForm</div>;
+  function onEmailSubmit(values: z.infer<typeof oldEmailSchema>) {
+    console.log(values);
+    otpForm.setValue('email', values.email);
+    setVerify(true);
+  }
+
+  function onVerifySubmit(values: z.infer<typeof otpSchema>) {
+    console.log(values);
+  }
+
+  return <Form {...emailForm}>
+    <form onSubmit={emailForm.handleSubmit(onEmailSubmit)} className="space-y-2">
+      <FormField
+        control={emailForm.control}
+        name='oldEmail'
+        render={({ field }) => (
+          <FormItem>
+            <Label>Current email</Label>
+            <FormControl>
+              <Input className='h-10 bg-secondary' disabled {...field} />
+            </FormControl>
+            <FormMessage className='text-xs text-red-500' />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={emailForm.control}
+        name='email'
+        render={({ field }) => (
+          <FormItem>
+            <Label>Enter a new email</Label>
+            <FormControl>
+              <Input placeholder='example@gmail.com' className='h-10 bg-secondary' {...field} />
+            </FormControl>
+            <FormMessage className='text-xs text-red-500' />
+          </FormItem>
+        )}
+      />
+
+      <Button type='submit' className='w-full'>
+        Verify email
+      </Button>
+    </form>
+  </Form>
 };
 
 export default EmailForm;
