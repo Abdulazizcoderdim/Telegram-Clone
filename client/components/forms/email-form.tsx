ffd654cddfefe460150eb68239469ae7cf7d260f
AbdulazizRus <coderdim@gmail.com>
2024-12-18 00:42:39 +0500

    email form

--- client/components/forms/email-form.tsx
+++ client/components/forms/email-form.tsx
@@ -1,4 +1,28 @@
+import { oldEmailSchema, otpSchema } from '@/lib/validation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
 const EmailForm = () => {
+  const [verify, setVerify] = useState(false);
+
+  const emailForm = useForm<z.infer<typeof oldEmailSchema>>({
+    resolver: zodResolver(oldEmailSchema),
+    defaultValues: {
+      email: '',
+      oldEmail: '',
+    },
+  });
+
+  const otpForm = useForm<z.infer<typeof otpSchema>>({
+    resolver: zodResolver(otpSchema),
+    defaultValues: {
+      email: '',
+      otp: '',
+    },
+  });
+
   return <div>EmaiForm</div>;
 };
 
