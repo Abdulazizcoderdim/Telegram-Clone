783e7a728def39952096a2394d35dda300b59dab
AbdulazizRus <coderdim@gmail.com>
2024-12-17 19:25:51 +0500

    information add

--- client/lib/validation.ts
+++ client/lib/validation.ts
@@ -4,6 +4,14 @@ export const emailSchema = z.object({
   email: z.string().email({ message: 'Please enter a valid email address.' }),
 });
 
+export const oldEmailSchema = z
+  .object({
+    oldEmail: z
+      .string()
+      .email({ message: 'Please enter a valid email address.' }),
+  })
+  .merge(emailSchema);
+
 export const otpSchema = z
   .object({
     otp: z.string().min(6, {
@@ -15,4 +23,10 @@ export const otpSchema = z
 export const messageSchema = z.object({
   text: z.string().min(1, { message: 'Please enter a message.' }),
   image: z.string().optional(),
+});
+
+export const profileSchema = z.object({
+  firstName: z.string().min(2),
+  lastName: z.string().optional(),
+  bio: z.string().optional(),
 });
