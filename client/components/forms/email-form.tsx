14ad0e2047b23c2a22701966cb55b55c1d564a82
AbdulazizRus <coderdim@gmail.com>
2024-12-19 18:23:05 +0500

    .

--- client/components/forms/email-form.tsx
+++ client/components/forms/email-form.tsx
@@ -4,13 +4,20 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { Button } from '../ui/button';
-import { Form, FormControl, FormField, FormItem, FormMessage } from '../ui/form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '../ui/form';
 import { Input } from '../ui/input';
 import { Label } from '../ui/label';
 
 const EmailForm = () => {
   const [verify, setVerify] = useState(false);
 
+  
   const emailForm = useForm<z.infer<typeof oldEmailSchema>>({
     resolver: zodResolver(oldEmailSchema),
     defaultValues: {
@@ -37,40 +44,49 @@ const EmailForm = () => {
     console.log(values);
   }
 
-  return <Form {...emailForm}>
-    <form onSubmit={emailForm.handleSubmit(onEmailSubmit)} className="space-y-2">
-      <FormField
-        control={emailForm.control}
-        name='oldEmail'
-        render={({ field }) => (
-          <FormItem>
-            <Label>Current email</Label>
-            <FormControl>
-              <Input className='h-10 bg-secondary' disabled {...field} />
-            </FormControl>
-            <FormMessage className='text-xs text-red-500' />
-          </FormItem>
-        )}
-      />
-      <FormField
-        control={emailForm.control}
-        name='email'
-        render={({ field }) => (
-          <FormItem>
-            <Label>Enter a new email</Label>
-            <FormControl>
-              <Input placeholder='example@gmail.com' className='h-10 bg-secondary' {...field} />
-            </FormControl>
-            <FormMessage className='text-xs text-red-500' />
-          </FormItem>
-        )}
-      />
+  return (
+    <Form {...emailForm}>
+      <form
+        onSubmit={emailForm.handleSubmit(onEmailSubmit)}
+        className="space-y-2"
+      >
+        <FormField
+          control={emailForm.control}
+          name="oldEmail"
+          render={({ field }) => (
+            <FormItem>
+              <Label>Current email</Label>
+              <FormControl>
+                <Input className="h-10 bg-secondary" disabled {...field} />
+              </FormControl>
+              <FormMessage className="text-xs text-red-500" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={emailForm.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <Label>Enter a new email</Label>
+              <FormControl>
+                <Input
+                  placeholder="example@gmail.com"
+                  className="h-10 bg-secondary"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="text-xs text-red-500" />
+            </FormItem>
+          )}
+        />
 
-      <Button type='submit' className='w-full'>
-        Verify email
-      </Button>
-    </form>
-  </Form>
+        <Button type="submit" className="w-full">
+          Verify email
+        </Button>
+      </form>
+    </Form>
+  );
 };
 
 export default EmailForm;
