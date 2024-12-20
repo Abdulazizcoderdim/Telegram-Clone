5581fa5d2abf19cb67a9a3506ef69a67db034093
AbdulazizRus <coderdim@gmail.com>
2024-12-21 00:58:33 +0500

    notification add

--- client/components/forms/notification-form.tsx
+++ client/components/forms/notification-form.tsx
@@ -1,12 +1,22 @@
+import useAudio from '@/hooks/use-audio';
 import { SOUNDS } from '@/lib/constants';
 import { cn } from '@/lib/utils';
 import { ChevronDown, Ghost, PlayCircle } from 'lucide-react';
 import { useState } from 'react';
 import { Button } from '../ui/button';
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
+import { Separator } from '../ui/separator';
+import { Switch } from '../ui/switch';
 
 const NotificationForm = () => {
   const [selectedSound, setSelectedSound] = useState('');
+
+  const { playSound } = useAudio();
+
+  const onPlaySound = (value: string) => {
+    setSelectedSound(value);
+    playSound(value);
+  };
 
   return (
     <>
@@ -35,7 +45,7 @@ const NotificationForm = () => {
                       'bg-primary-foreground ': selectedSound === sound.value,
                     }
                   )}
-                  onClick={() => setSelectedSound(sound.value)}
+                  onClick={() => onPlaySound(sound.value)}
                 >
                   <Button
                     size={'sm'}
@@ -44,7 +54,7 @@ const NotificationForm = () => {
                   >
                     {sound.label}
                   </Button>
-                  {true ? (
+                  {selectedSound === sound.value ? (
                     <Button size={'icon'}>
                       <Ghost />
                     </Button>
@@ -59,6 +69,71 @@ const NotificationForm = () => {
             </div>
           </PopoverContent>
         </Popover>
+      </div>
+
+      <Separator className="my-3" />
+
+      <div className="flex items-center justify-between relative">
+        <div className="flex flex-col">
+          <p className="font-spaceGrotesk">Sending Sound</p>
+          <p className="font-spaceGrotesk text-muted-foreground text-xs">
+            Apple
+          </p>
+        </div>
+
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button size={'sm'}>
+              Select <ChevronDown />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-80 absolute -right-12">
+            <div className="flex flex-col space-y-1">
+              {SOUNDS.map(sound => (
+                <div
+                  key={sound.label}
+                  className={cn(
+                    'flex justify-between items-center bg-secondary cursor-pointer hover:bg-primary-foreground',
+                    {
+                      'bg-primary-foreground ': selectedSound === sound.value,
+                    }
+                  )}
+                  onClick={() => onPlaySound(sound.value)}
+                >
+                  <Button
+                    size={'sm'}
+                    variant={'ghost'}
+                    className="justify-start"
+                  >
+                    {sound.label}
+                  </Button>
+                  {selectedSound === sound.value ? (
+                    <Button size={'icon'}>
+                      <Ghost />
+                    </Button>
+                  ) : (
+                    <Button size={'icon'} variant={'ghost'}>
+                      <PlayCircle />
+                    </Button>
+                  )}
+                </div>
+              ))}
+              <Button className="w-full mt-2 font-bold">Submit</Button>
+            </div>
+          </PopoverContent>
+        </Popover>
+      </div>
+
+      <Separator className="my-3" />
+
+      <div className="flex items-center justify-between relative">
+        <div className="flex flex-col">
+          <p>Mode Mute</p>
+          <p className="text-muted-foreground text-xs">
+            {true ? 'Muted' : 'Unmuted'}
+          </p>
+        </div>
+        <Switch />
       </div>
     </>
   );
---
+++ client/hooks/use-audio.ts
@@ -0,0 +1,33 @@
+import useSound from 'use-sound';
+
+const useAudio = () => {
+  const [play1] = useSound('/audio/notification.mp3');
+  const [play2] = useSound('/audio/notification2.mp3');
+  const [play3] = useSound('/audio/sending.mp3');
+  const [play4] = useSound('/audio/sending2.mp3');
+
+  const playSound = (value: string) => {
+    switch (value) {
+      case 'notification.mp3':
+        play1();
+        break;
+      case 'notification2.mp3':
+        play2();
+        break;
+      case 'sending.mp3':
+        play3();
+        break;
+      case 'sending2.mp3':
+        play4();
+        break;
+      default:
+        break;
+    }
+  };
+
+  return {
+    playSound,
+  };
+};
+
+export default useAudio;
