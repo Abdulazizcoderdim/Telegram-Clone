1ef2e4bb9d44469f4d2000b0376ebbd9a96e51c4
AbdulazizRus <coderdim@gmail.com>
2024-12-20 20:38:21 +0500

    notificatio

--- client/components/forms/notification-form.tsx
+++ client/components/forms/notification-form.tsx
@@ -1,5 +1,27 @@
+import { ChevronDown } from "lucide-react";
+import { Popover, PopoverTrigger } from "../ui/popover";
+
 const NotificationForm = () => {
-  return <div>NotificationForm</div>;
+  return ( 
+    <>
+      <div className='flex items-center justify-between relative'>
+        <div className='flex flex-col'>
+					<p className='font-spaceGrotesk'>Notification Sound</p>
+					<p className='font-spaceGrotesk text-muted-foreground text-xs'>
+            Apple
+          </p>
+				</div>
+
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button>
+              Select <ChevronDown/>
+            </Button>
+            </PopoverTrigger>
+        </Popover>
+      </div>
+    </>
+  )
 };
 
 export default NotificationForm;
