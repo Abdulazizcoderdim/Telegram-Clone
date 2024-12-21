e83b11f7db7f6eddabfe91a754a3bb7ee5891163
AbdulazizRus <coderdim@gmail.com>
2024-12-21 11:38:44 +0500

    server strated on port 3000

---
+++ server/app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+// Routes
+app.use('/api', require('./routes/index'));
+
+const bootstrap = async () => {
+  try {
+    const PORT = process.env.PORT || 6000;
+    mongoose.connect(process.env.MONGO_URL).then(() => {
+      console.log('Connected to MongoDB ✔✔✔');
+      app.listen(PORT, () => {
+        console.log(`Server is running on port http://localhost:${PORT}`);
+      });
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+bootstrap();
