2118198136e028d4f7805c0426903a36dd29ca8d
AbdulazizRus <coderdim@gmail.com>
2024-12-21 21:46:13 +0500

    change

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,8 +1,19 @@
+const userModel = require('../model/user.model');
+
 class AuthController {
   async login(req, res, next) {
-    const { email } = req.body;
-
-    res.json({ email });
+    try {
+      const { email } = req.body;
+      const createdUser = await userModel.create({ email });
+      res.status(201).json(createdUser);
+    } catch (error) {
+      res
+        .status(500)
+        .json({
+          message:
+            error.message || 'An error occurred while creating the user.',
+        });
+    }
   }
 
   async verify(req, res, next) {
