0bd262ec1cac013ad09d0114f0a9fce94785ec3e
AbdulazizRus <coderdim@gmail.com>
2025-01-04 10:38:47 +0500

    session

--- client/lib/auth-options.ts
+++ client/lib/auth-options.ts
@@ -1,5 +1,7 @@
+import User from '@/models/user.model';
 import { NextAuthOptions } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+import { connectToDatabase } from './mongoose';
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -7,12 +9,27 @@ export const authOptions: NextAuthOptions = {
       name: 'Credentials',
       credentials: { email: { label: 'Email', type: 'email' } },
       async authorize(credentials) {
-        return null;
+        await connectToDatabase();
+        const user = await User.findOne({ email: credentials?.email });
+        return user;
       },
     }),
   ],
   callbacks: {
     async session({ session }) {
+      await connectToDatabase();
+      const isExistingUser = await User.exists({ email: session.user?.email });
+
+      if (isExistingUser) {
+        const user = await User.create({
+          email: session.user?.email,
+          isVerified: true,
+          avatar: session.user?.image,
+        });
+        session.currentUser = user;
+        return session;
+      }
+      session.currentUser = isExistingUser;
       return session;
     },
   },
