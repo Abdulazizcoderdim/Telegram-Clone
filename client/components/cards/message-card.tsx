cdd6b2f6a8714d254f67562905421f71bcb372fa
AbdulazizRus <coderdim@gmail.com>
2025-01-29 19:25:24 +0500

    message card right click added

--- client/components/cards/message-card.tsx
+++ client/components/cards/message-card.tsx
@@ -1,3 +1,9 @@
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from '@/components/ui/context-menu';
 import { useCurrentContact } from '@/hooks/use-current';
 import { CONST } from '@/lib/constants';
 import { cn } from '@/lib/utils';
@@ -16,36 +22,46 @@ const MessageCard: FC<Props> = ({ message }) => {
   console.log(currentContact);
 
   return (
-    <div
-      className={cn(
-        'm-2.5 font-medium text-xs flex',
-        message.receiver._id === currentContact?._id
-          ? 'justify-start'
-          : 'justify-end'
-      )}
-    >
-      <div
-        className={cn(
-          'relative dark:text-white text-black rounded-md inline p-2 pl-2.5 max-w-full pr-12',
-          message.receiver._id === currentContact?._id
-            ? 'bg-primary text-white'
-            : 'bg-secondary'
-        )}
-      >
-        <p className="text-sm font-sans">{message.text}</p>
-        <div className="right-1 bottom-0 opacity-60 absolute text-[9px] flex gap-[3px]">
-          <p className='font-sans'>{format(message.updatedAt, 'hh:mm')}</p>
-          <div className="self-end">
-            {message.receiver._id === currentContact?._id &&
-              (message.status === CONST.READ ? (
-                <CheckCheck size={12} />
-              ) : (
-                <Check size={12} />
-              ))}
+    <ContextMenu>
+      <ContextMenuTrigger asChild>
+        <div
+          className={cn(
+            'm-2.5 font-medium text-xs flex',
+            message.receiver._id === currentContact?._id
+              ? 'justify-start'
+              : 'justify-end'
+          )}
+        >
+          <div
+            className={cn(
+              'relative dark:text-white text-black rounded-md inline p-2 pl-2.5 max-w-full pr-12',
+              message.receiver._id === currentContact?._id
+                ? 'bg-primary text-white'
+                : 'bg-secondary'
+            )}
+          >
+            <p className="text-sm font-sans">{message.text}</p>
+            <div className="right-1 bottom-0 opacity-60 absolute text-[9px] flex gap-[3px]">
+              <p className="font-sans">{format(message.updatedAt, 'hh:mm')}</p>
+              <div className="self-end">
+                {message.receiver._id === currentContact?._id &&
+                  (message.status === CONST.READ ? (
+                    <CheckCheck size={12} />
+                  ) : (
+                    <Check size={12} />
+                  ))}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </ContextMenuTrigger>
+      <ContextMenuContent>
+        <ContextMenuItem>Profile</ContextMenuItem>
+        <ContextMenuItem>Billing</ContextMenuItem>
+        <ContextMenuItem>Team</ContextMenuItem>
+        <ContextMenuItem>Subscription</ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
   );
 };
 
