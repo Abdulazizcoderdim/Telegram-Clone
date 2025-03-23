d5aa6c2b2c80cb52f30a8b2ad534009befd3fd2b
AbdulazizRus <coderdim@gmail.com>
2025-03-23 14:17:35 +0500

    change

--- client/app/auth/_component/sign-in.tsx
+++ client/app/auth/_component/sign-in.tsx
@@ -1,45 +1,45 @@
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
   FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { useAuth } from '@/hooks/use-auth';
-import { toast } from '@/hooks/use-toast';
-import { axiosClient } from '@/http/axios';
-import { emailSchema } from '@/lib/validation';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useMutation } from '@tanstack/react-query';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useAuth } from "@/hooks/use-auth";
+import { toast } from "@/hooks/use-toast";
+import { axiosClient } from "@/http/axios";
+import { emailSchema } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 const SignIn = () => {
   const { setEmail, setStep } = useAuth();
   const form = useForm<z.infer<typeof emailSchema>>({
     resolver: zodResolver(emailSchema),
     defaultValues: {
-      email: '',
+      email: "",
     },
   });
 
   const { mutate, isPending } = useMutation({
     mutationFn: async (email: string) => {
       const { data } = await axiosClient.post<{ email: string }>(
-        '/api/auth/login',
+        "/api/auth/login",
         { email }
       );
       return data;
     },
-    onSuccess: res => {
+    onSuccess: (res) => {
       setEmail(res.email);
-      setStep('verify');
+      setStep("verify");
       toast({
         description:
-          'We have sent you an email with a verification code to your email address.',
+          "We have sent you an email with a verification code to your email address.",
       });
     },
   });
@@ -78,9 +78,9 @@ const SignIn = () => {
             disabled={isPending}
             type="submit"
             className="w-full"
-            size={'lg'}
+            size={"lg"}
           >
-            Submit
+            {isPending ? "Loading..." : "Submit"}
           </Button>
         </form>
       </Form>
